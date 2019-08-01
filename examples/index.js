43bf6b9cdfc66a82b793d2b17da576bae8ccde09
yuzexing <752091109@qq.com>
2019-08-01 22:56:34 +0800

    Add files via upload

---
+++ examples/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/Scroll'
+import './styles.css'
+
+//ReactDOM.unstable_createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ examples/webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const fs = require('fs')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const ShakePlugin = require('webpack-common-shake').Plugin
+
+const createAlias = (name, fallback) =>
+  fs.existsSync(`./../../${name}`) ? path.resolve(`./../../${name}`) : fallback || name
+
+module.exports = mode => {
+  return {
+    mode,
+    entry: 'index.js',
+    output: { filename: 'bundle.js', path: path.resolve('./dist') },
+    module: {
+      rules: [
+        { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+        {
+          test: /\.(js|jsx|tsx|ts)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    modules: false,
+                    loose: true,
+                    useBuiltIns: false,
+                    targets: { browsers: 'last 2 Chrome versions' },
+                  },
+                ],
+                '@babel/preset-react',
+                '@babel/preset-typescript',
+              ],
+              plugins: [
+                '@babel/plugin-syntax-dynamic-import',
+                ['@babel/plugin-proposal-class-properties', { loose: true }],
+              ],
+            },
+          },
+        },
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          use: ['file-loader', { loader: 'image-webpack-loader' }],
+        },
+      ],
+    },
+    resolve: {
+      modules: [path.resolve('./'), 'node_modules'],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      alias: {
+        'react-three-fiber': path.resolve(`./../index`),
+        //scheduler: path.resolve('node_modules/scheduler'),
+        react: path.resolve('node_modules/react'),
+        'react-dom': path.resolve('node_modules/react-dom'),
+        'prop-types': path.resolve('node_modules/prop-types'),
+        three$: path.resolve('node_modules/three/src/Three'),
+        //three$: path.resolve('./resources/three.js'),
+        lodash: path.resolve('../node_modules/lodash-es'),
+        'lodash-es': path.resolve('../node_modules/lodash-es'),
+        'react-spring/three': createAlias('react-spring/src/targets/three', 'react-spring/three'),
+        'react-use-gesture': createAlias('react-use-gesture/index.js', 'react-use-gesture'),
+        //'pointer-events-polyfill': createAlias('pointer-events-polyfill/dist/pep.js', 'pointer-events-polyfill'),
+      },
+    },
+    optimization: {
+      splitChunks: {
+        // include all types of chunks
+        chunks: 'all',
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: 'public/index.html' }),
+      new ShakePlugin(),
+      ...(mode === 'production' ? [new BundleAnalyzerPlugin()] : []),
+    ],
+    devServer: { hot: false, contentBase: path.resolve('./'), stats: 'errors-only' },
+    devtool: undefined,
+    performance: { hints: false },
+  }
+}
