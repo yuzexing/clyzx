1505b11cc73dc7cf3be909531d385f5260beb434
yuzexing <752091109@qq.com>
2019-08-01 22:47:01 +0800

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,34 @@
+/// <reference path="types/three.d.ts" />
+
+import { Canvas } from './src/canvas'
+import { useRender, useThree, useUpdate, useResource } from './src/hooks'
+import {
+  addEffect,
+  invalidate,
+  render,
+  unmountComponentAtNode,
+  extend,
+  applyProps,
+  createPortal,
+} from './src/reconciler'
+
+const apply = (args: any) => {
+  console.warn('react-three-fiber: Please use extend ✅ instead of apply ❌, the former will be made obsolete soon!')
+  extend(args)
+}
+
+export {
+  Canvas,
+  addEffect,
+  invalidate,
+  render,
+  createPortal,
+  unmountComponentAtNode,
+  apply,
+  extend,
+  applyProps,
+  useRender,
+  useThree,
+  useUpdate,
+  useResource,
+}
---
+++ types/index.d.ts
@@ -0,0 +1,28 @@
+/// <reference path="three.d.ts" />
+import { Canvas } from './src/canvas'
+import { useRender, useThree, useUpdate, useResource } from './src/hooks'
+import {
+  addEffect,
+  invalidate,
+  render,
+  unmountComponentAtNode,
+  extend,
+  applyProps,
+  createPortal,
+} from './src/reconciler'
+declare const apply: (args: any) => void
+export {
+  Canvas,
+  addEffect,
+  invalidate,
+  render,
+  createPortal,
+  unmountComponentAtNode,
+  apply,
+  extend,
+  applyProps,
+  useRender,
+  useThree,
+  useUpdate,
+  useResource,
+}
